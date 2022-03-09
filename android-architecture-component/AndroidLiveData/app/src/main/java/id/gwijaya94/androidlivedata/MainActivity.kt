059d6b136b1d7d5305d4635364ac93bb619c7826
Gilbert <gilbert.wijaya@ai.astra.co.id>
2022-03-10 01:49:07 +0700

    - create MainViewModel to set timer live data - add lib activity android-ktx, lifecycle viewmodel and lifecycle livedata on app/build.gradle - implement MainViewModel to set timer on MainActivity

--- android-architecture-component/AndroidLiveData/app/src/main/java/id/gwijaya94/androidlivedata/MainActivity.kt
+++ android-architecture-component/AndroidLiveData/app/src/main/java/id/gwijaya94/androidlivedata/MainActivity.kt
@@ -2,10 +2,27 @@ package id.gwijaya94.androidlivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import id.gwijaya94.androidlivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private val mLiveDataTimerViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
