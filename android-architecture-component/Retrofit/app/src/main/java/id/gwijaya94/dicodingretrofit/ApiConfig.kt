87f1538916436a6ab0acff846d89104ae842b51d
Gilbert <gilbert.wijaya@ai.astra.co.id>
2022-03-10 02:10:46 +0700

    add Retrofit project for viewmodel integration

---
+++ android-architecture-component/Retrofit/app/src/main/java/id/gwijaya94/dicodingretrofit/ApiConfig.kt
@@ -0,0 +1,26 @@
+package id.gwijaya94.dicodingretrofit
+
+import android.content.Context
+import androidx.viewbinding.BuildConfig
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        fun getApiService(context: Context): ApiService {
+//            val loggingInterceptor = if (BuildConfig.DEBUG) {
+//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+//            } else {
+//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+//            }
+            val client = OkHttpClient.Builder().addInterceptor(ChuckerInterceptor(context)).build()
+            val retrofit = Retrofit.Builder().baseUrl("https://restaurant-api.dicoding.dev")
+                .addConverterFactory(GsonConverterFactory.create()).client(client).build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
---
+++ android-architecture-component/Retrofit/app/src/main/java/id/gwijaya94/dicodingretrofit/ApiService.kt
@@ -0,0 +1,18 @@
+package id.gwijaya94.dicodingretrofit
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("/detail/{id}")
+    fun getRestaurant(@Path("id") id: String): Call<ResponseRestaurant>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
+}
