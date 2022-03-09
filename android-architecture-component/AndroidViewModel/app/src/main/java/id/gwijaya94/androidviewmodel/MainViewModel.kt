e6fed2a3f1c753461481a0740ce0ac8d135988e3
Gilbert <gilbert.wijaya@ai.astra.co.id>
2022-03-10 00:07:17 +0700

    - setup MainViewModel - setup main activity page - implement MainViewModel in MainActivity

---
+++ android-architecture-component/AndroidViewModel/app/src/main/java/id/gwijaya94/androidviewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package id.gwijaya94.androidviewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var result = 0
+
+    fun calculate(width: String, height: String, length: String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
+}
