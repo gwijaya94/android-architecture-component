1967ef47e7cbef25e972cdbc7e52ec214d31f35b
Gilbert <gilbert.wijaya@ai.astra.co.id>
2022-03-13 12:52:23 +0700

    config implement viewmodel both on DetailUser and Main Acticity

--- submission/MyGitHubUsersApp/app/src/main/java/id/gwijaya94/mygithubusersapp/activity/DetailUserActivity.kt
+++ submission/MyGitHubUsersApp/app/src/main/java/id/gwijaya94/mygithubusersapp/activity/DetailUserActivity.kt
@@ -1,18 +1,26 @@
 package id.gwijaya94.mygithubusersapp.activity
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import id.gwijaya94.mygithubusersapp.R
 import id.gwijaya94.mygithubusersapp.databinding.ActivityDetailUserBinding
-import id.gwijaya94.mygithubusersapp.model.User
+import id.gwijaya94.mygithubusersapp.model.DetailViewModel
+import id.gwijaya94.mygithubusersapp.model.UserDetail
+import id.gwijaya94.mygithubusersapp.setHeaderColor
 
 class DetailUserActivity : AppCompatActivity() {
     companion object {
-        const val USER_DATA = "user_data"
+        const val USER_NAME = "gwijaya94"
     }
 
+    private val detailViewModel: DetailViewModel by viewModels()
     private lateinit var binding: ActivityDetailUserBinding
-    private lateinit var userData: User
+    private lateinit var userData: UserDetail
+    private var headerColor = R.color.github_header
 
     override fun onSupportNavigateUp(): Boolean {
         finish()
@@ -21,32 +29,63 @@ class DetailUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val userNameExtra = intent.getStringExtra(USER_NAME) as String
+        detailViewModel.setContext(this, userNameExtra)
         binding = ActivityDetailUserBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        userData = intent.getParcelableExtra<User>(USER_DATA) as User
 
-        supportActionBar?.apply {
-            title = userData.username.lowercase()
-            setDisplayHomeAsUpEnabled(true)
+        detailViewModel.getUserDetail()
+        setContentView(binding.root)
+        setUp(userNameExtra)
+
+        detailViewModel.selectedData.observe(this) {
+            if (it != null) {
+                userData = it
+                setUser(it)
+                binding.scrollView2.visibility = View.VISIBLE
+            } else binding.scrollView2.visibility = View.GONE
+        }
+        detailViewModel.isLoading.observe(this) {
+            if (it) binding.progressBar.visibility = View.VISIBLE
+            else binding.progressBar.visibility = View.INVISIBLE
+        }
+        detailViewModel.errorData.observe(this) {
+            if (it != null) binding.scrollView2.visibility = android.view.View.GONE
+            else binding.scrollView2.visibility = android.view.View.VISIBLE
+        }
+        detailViewModel.followers.observe(this) {
+
+        }
+        detailViewModel.following.observe(this) {
+
         }
 
-        val imgResource = resources.getIdentifier(userData.avatar, null, packageName)
+
+    }
+
+    private fun setUser(userData: UserDetail) {
+        val imgResource = userData.avatarUrl
         Glide.with(this)
             .load(imgResource)
             .circleCrop()
             .into(binding.profilePicture)
 
         binding.apply {
-            userName.text = userData.name
-            userCompany.text = userData.company
-            userLocation.text = userData.location
-            userUsername.text = "@${userData.username.lowercase()}"
-            userInfo.followerCountValue.text = userData.follower
-            userInfo.followingCountValue.text = userData.following
-            userInfo.repoCountValue.text = userData.repository
-
+            userName.text = userData.name ?: userData.login
+            userUsername.text = "ID ${userData.id?.toString()}"
+            userBio.text = userData.bio ?: "-"
+            userInfo.followerCountValue.text = userData.followers.toString()
+            userInfo.followingCountValue.text = userData.following.toString()
+            userInfo.repoCountValue.text = userData.publicRepos.toString()
         }
+    }
 
+    private fun setUp(userNameExtra: String) {
+        supportActionBar?.apply {
+            title = "@${userNameExtra.lowercase()}"
+            setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.github_header)))
+            setDisplayHomeAsUpEnabled(true)
+        }
+        setHeaderColor(this, window, headerColor)
     }
 
 }
--- submission/MyGitHubUsersApp/app/src/main/java/id/gwijaya94/mygithubusersapp/activity/MainActivity.kt
+++ submission/MyGitHubUsersApp/app/src/main/java/id/gwijaya94/mygithubusersapp/activity/MainActivity.kt
@@ -2,39 +2,79 @@ package id.gwijaya94.mygithubusersapp.activity
 
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.gwijaya94.mygithubusersapp.ListUserAdapter
+import id.gwijaya94.mygithubusersapp.R
 import id.gwijaya94.mygithubusersapp.databinding.ActivityMainBinding
 import id.gwijaya94.mygithubusersapp.getIcon
-import id.gwijaya94.mygithubusersapp.getJsonDataFromAsset
-import id.gwijaya94.mygithubusersapp.model.User
+import id.gwijaya94.mygithubusersapp.model.GithubUser
+import id.gwijaya94.mygithubusersapp.model.MainViewModel
+import id.gwijaya94.mygithubusersapp.setHeaderColor
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var listUserData: List<User>
+    private val mainViewModel: MainViewModel by viewModels()
+    private var headerColor = R.color.github_header
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupApp()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        mainViewModel.isLoading.observe(this) {
+            if (it) {
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
+        mainViewModel.searchData.observe(this) {
+            val listData = ArrayList<GithubUser>()
+            if (it != null) {
+                for (data in it) {
+                    if (data != null) listData.add(data)
+                }
+            }
+            setListData(listData)
+        }
+        mainViewModel.errorData.observe(this) {
+            binding.errText.apply {
+                if (it != null && it != "") {
+                    text = it
+                    visibility = View.VISIBLE
+                } else visibility = View.INVISIBLE
+            }
+        }
+        mainViewModel.getSearchData("gwija")
+    }
+
+    private fun setupApp() {
+        mainViewModel.setContext(this)
+        setHeaderColor(this, window, headerColor)
         supportActionBar?.apply {
             title = "Github Users"
             setHomeAsUpIndicator(getIcon(this@MainActivity, "cmd_github", color = Color.WHITE))
             setDisplayHomeAsUpEnabled(true)
+            setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.github_header)))
         }
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    }
 
-        var jsonListUser = getJsonDataFromAsset(this, "githubuser.json")
-        listUserData = jsonListUser.users
-
+    private fun setListData(listUserData: List<GithubUser>) {
         val listUsersAdapter = ListUserAdapter(listUserData)
         listUsersAdapter.setOnItemClicked(object : ListUserAdapter.OnItemClickCallback {
-            override fun onItemClicked(data: User) {
+            override fun onItemClicked(data: GithubUser) {
                 val moveDetailUserActivity =
                     Intent(this@MainActivity, DetailUserActivity::class.java)
-                moveDetailUserActivity.putExtra(DetailUserActivity.USER_DATA, data)
+                moveDetailUserActivity.putExtra(DetailUserActivity.USER_NAME, data.login)
                 startActivity(moveDetailUserActivity)
             }
         })
@@ -44,4 +84,5 @@ class MainActivity : AppCompatActivity() {
             adapter = listUsersAdapter
         }
     }
+
 }
